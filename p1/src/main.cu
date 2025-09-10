fcceb9438d78a7db0de22f4cf3ecb791c8d991b9
Mshkss <tolya200059@gmail.com>
2025-09-10 18:29:01 +0700

    12

--- p1/src/main.cu
+++ p1/src/main.cu
@@ -2,11 +2,11 @@
 
 // CUDA kernel — выполняется на GPU
 __global__ void cuda_hello() { printf("Hello World from GPU1!\n"); }
-
+ 
 int main() {
+
     // Запуск kernel: 1 блок, 1 поток
     cuda_hello<<<1, 1>>>();
-
     // Ждём завершения всех потоков GPU перед завершением программы
     cudaDeviceSynchronize();
 
@@ -16,7 +16,6 @@ int main() {
         printf("CUDA Error: %s\n", cudaGetErrorString(err));
         return 1;
     }
-
     printf("Hello World from CPU1!\n");
     return 0;
 }
